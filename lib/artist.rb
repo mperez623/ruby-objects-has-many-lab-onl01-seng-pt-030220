f2b099ee64a4a7d2481956a635543786e108d579
Melissa Perez <melissaperezg.96@gmail.com>
2020-04-06 22:00:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,13 +9,15 @@ class Artist
     @songs = []
   end
   
-  def add_song(song)
-    song.artist = self
-  end
-  
   def songs
     Song.all.select { |song| song.artist == self } 
   end
+  
+   def add_song(song)
+    song.artist = self
+  end
+  
+  
   
 
   
