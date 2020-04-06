5bc9b7ed2d6cd34aca57e242a01150bc065393f4
Melissa Perez <melissaperezg.96@gmail.com>
2020-04-06 22:44:58 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,9 +11,9 @@ class Author
     Post.all.select {|post| post.author == self}
   end
   
-  # def add_post(post)
-  #   post.author = self
-  # end
+  def add_post(post)
+    post.author = self
+  end
   
   
   
