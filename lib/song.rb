75cb1d481e5642a9603217079e3d4f59f1a760f8
Melissa Perez <melissaperezg.96@gmail.com>
2020-04-06 22:50:10 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song
+  
+  @@all = []
+  
+  attr_accessor :artist, :name
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def artist_name
+    if self.artist == nil
+      nil
+    else
+      self.artist.name
+    end
+  end
+  
+  
+  
+end
